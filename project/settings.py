2dc2fe0d45a19efb503f41b27c55cc8aa3e1ba54
Kenno-Warise <wariken0523@gmail.com>
2022-06-15 09:45:41 +0900

    mediaディレクトリを有効にする

--- project/settings.py
+++ project/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -144,7 +144,7 @@ try:
 
     # MEDIA_URL = '/media/'
 
-    MEDIA_ROOT = BASE_DIR / 'media'
+    # MEDIA_ROOT = BASE_DIR / 'media'
 except:
     pass
     
