a8fa492ba397d2c9c3ffa7dfb90ecc18ba9ea6f5
Kenno-Warise <wariken0523@gmail.com>
2022-06-24 14:46:31 +0900

    dropboxのトークンを空にした

--- project/settings.py
+++ project/settings.py
@@ -150,7 +150,7 @@ if not DEBUG:
     import os
 
     DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-    DROPBOX_OAUTH2_TOKEN = os.environ['DROPBOX_TOKEN'] # Herokuの環境変数から取得
+    DROPBOX_OAUTH2_TOKEN = '' # os.environ['DROPBOX_TOKEN'] # Herokuの環境変数から取得
     DROPBOX_ROOT_PATH = '/media/'
 
 # heroku settings. DATABASE, SECRET_KEY, STATIC_ROOT, WhiteNose Aout Set
