49d9a9b0cec5a530a203af3f9f1c2b61ed9e09bd
Kenno-Warise <wariken0523@gmail.com>
2022-06-24 15:12:28 +0900

    更新トークンを確実に代入

--- todo/management/commands/dropbox_setup.py
+++ todo/management/commands/dropbox_setup.py
@@ -11,11 +11,13 @@ class Command(BaseCommand):
             try:
                 print(default_storage.client.users_get_current_account().name.display_name, '完了')
             except:
-                oauth_refresh_token = default_storage.client._oauth2_access_token
                 default_storage.client._oauth2_access_token = ''
+
+                oauth_refresh_token = settings.DROPBOX_REFRESH_TOKEN
                 dropbox_key = settings.DROPBOX_KEY
+                default_storage.client._oauth2_refresh_token = oauth_refresh_token
                 default_storage.client._app_key = dropbox_key
-                print(default_storage.client.__dict__)
-                # print(default_storage.client.users_get_current_account().name.display_name, 'セットアップ完了')
+
+                print(default_storage.client.users_get_current_account().name.display_name, 'セットアップ完了')
         else:
             print('デバッグは有効です')
