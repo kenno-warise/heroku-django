61fc5cdb059b726c482af95ab4f306d80798c41d
Kenno-Warise <wariken0523@gmail.com>
2022-06-24 15:07:05 +0900

    カスタムコマンドの修正

--- todo/management/commands/dropbox_setup.py
+++ todo/management/commands/dropbox_setup.py
@@ -15,6 +15,7 @@ class Command(BaseCommand):
                 default_storage.client._oauth2_access_token = ''
                 dropbox_key = settings.DROPBOX_KEY
                 default_storage.client._app_key = dropbox_key
-                print(default_storage.client.users_get_current_account().name.display_name, 'セットアップ完了')
+                print(default_storage.client.__dict__)
+                # print(default_storage.client.users_get_current_account().name.display_name, 'セットアップ完了')
         else:
             print('デバッグは有効です')
