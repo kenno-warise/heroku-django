6ce76434dd9e78f19ca152627ccc6f273bf7d06f
Kenno-Warise <wariken0523@gmail.com>
2022-06-25 08:01:50 +0900

    ファイル保存時のオーバーライド

--- todo/admin.py
+++ todo/admin.py
@@ -1,25 +1,10 @@
 from django.contrib import admin
-from django.core.files.storage import default_storage
-from django.conf import settings
 
 from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'file_dir', 'created_at')
-
-    def save_model(self, request, obj, form, change):
-        if not settings.DEBUG:
-            if obj.file_dir:
-                print('file=True')
-                default_storage.client._oauth2_access_token = ''
-                default_storage.client._oauth2_refresh_token = settings.DROPBOX_OAUTH2_TOKEN
-                default_storage.client._app_key = settings.DROPBOX_KEY
-                print(default_storage.client.__dict__)
-            else:
-                print('file=False')
-
-        super(PostAdmin, self).save_model(request, obj, form, change)
 
 
 admin.site.register(Post, PostAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
+from django.core.files.storage import default_storage
+from django.conf import settings
 
 
 class Post(models.Model):
@@ -12,6 +14,21 @@ class Post(models.Model):
             blank=True
     )
     created_at = models.DateTimeField('作成日', auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        """
+        ファイルが保存される前の処理
+        Dropboxのアクセストークンを更新する
+        """
+        if not settings.DEBUG:
+            if self.file_dir:
+                print('file=True')
+                default_storage.client._oauth2_access_token = ''
+                default_storage.client._oauth2_refresh_token = settings.DROPBOX_OAUTH2_TOKEN
+                default_storage.client._app_key = settings.DROPBOX_KEY
+            else:
+                print('flie=False')
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
