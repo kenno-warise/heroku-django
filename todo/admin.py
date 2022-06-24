238dc5c026c9b5d9943516a443ec3b4746bba589
Kenno-Warise <wariken0523@gmail.com>
2022-06-24 15:21:46 +0900

    adminを編集

--- todo/admin.py
+++ todo/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.files.storage import default_storage
 from django.conf import settings
 
 from .models import Post
@@ -8,14 +9,12 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'file_dir', 'created_at')
 
     def save_model(self, request, obj, form, change):
-        if obj.file_dir:
-            print('file=True')
-            if settings.DEBUG:
-                print('DEBUG=True')
+        if not settings.DEBUG:
+            if obj.file_dir:
+                print('file=True')
+                print(default_storage.client.__dict__)
             else:
-                print('DEBUG=False')
-        else:
-            print('file=False')
+                print('file=False')
 
         super(PostAdmin, self).save_model(request, obj, form, change)
 
