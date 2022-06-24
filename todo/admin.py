b3774828461c944afbf1cec0323ad795cd0f1978
Kenno-Warise <wariken0523@gmail.com>
2022-06-25 08:20:58 +0900

    adminサイトが不調

--- todo/admin.py
+++ todo/admin.py
@@ -3,8 +3,4 @@ from django.contrib import admin
 from .models import Post
 
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'file_dir', 'created_at')
-
-
 admin.site.register(Post, PostAdmin)
